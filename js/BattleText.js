fe693d6ee9e16c62beb7622c74b48e86feba64df
uxff <uxff@qq.com>
2016-03-18 23:18:03 +0800

    add battle text for dmg

---
+++ js/BattleText.js
@@ -0,0 +1,61 @@
+function BattleText(x, y, str, type)
+{ 
+	Sprite.call(this, x, y, "score", type);
+	
+	this.str = str;
+    this.type = type;
+	this.time = 0;
+    this.fontSize = 16;
+}
+
+BattleText.prototype = new Sprite();
+
+BattleText.prototype.draw = function (canvas)
+{
+	var myCanvas = document.getElementById(canvas);
+	var graphics = myCanvas.getContext("2d");
+	
+	//graphics.drawImage(img, images[this.src][0], sc + images[this.src][1], 28, 14, this.x + offerX  - 14, this.y + offerY  - 7, 28, 14 ) ;	
+
+    
+    graphics.font = this.fontSize+"px Courier New";
+    graphics.fillStyle = 'rgb(255,'+(255-this.type*80)+',20)';
+    // 必须添加baseline属性 不然擦除不了 决定绘出文字的位置
+    graphics.textBaseline = 'top';
+    graphics.fillText(this.str, this.x + offerX - 10, this.y + offerY - 7);
+	
+	return;
+};
+
+BattleText.prototype.updata = function()
+{
+	this.time ++;
+};
+
+BattleText.prototype.clear = function(canvas)
+{
+	var myCanvas = document.getElementById(canvas);
+	var graphics = myCanvas.getContext("2d");
+	
+	graphics.clearRect(this.x + offerX - 10, this.y + offerY - 7, graphics.measureText(this.str).width, this.fontSize);
+};
+
+
+function drawBattleTexts()
+{
+	for(var i = 0;i < battleTexts.length; i ++)
+	{
+		battleTexts[i].draw("upp");
+		battleTexts[i].updata();
+		
+		if(battleTexts[i].time >30 )
+		{
+			battleTexts[i].clear("upp");
+			
+			battleTexts.splice(i,1);
+			i --;
+		}
+	}
+}
+
+
